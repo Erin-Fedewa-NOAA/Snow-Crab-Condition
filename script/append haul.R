39c488d2d8f810113170cf8bea22054bd6a45b32
Erin-Fedewa-NOAA <Erin.Fedewa@nmfs.local>
2023-02-24 19:14:49 -0900

    Wohoooo we've got fatty acid data!

--- script/append haul.R
+++ script/append haul.R
@@ -1,21 +1,22 @@
 #GOALS: 
-# 1) Append haul data to master condition datasheet
+# 1) Append haul data to snow crab biometrics datasheet
 #2) Add a column for maturity using clutch codes/chela heights
     #Distribution based cutlines for males published in 2019 tech memo
 #3) Calculate CPUE at each station
+#4) Join haul data with fatty acid data 
  
 # Author: Erin Fedewa
-# last updated: 1/13/23
+# last updated: 2/20/23
 
 # load ----
 library(tidyverse)
 
 #############################
-#Append maturity 
-condition_master <- read.csv("./data/Snow Condition Master.csv")
+#Append maturity to biometrics data
+bio_dat <- read.csv("./data/2019_2022 biometrics data.csv")
 
 #Determine male maturity via distribution-based cutline method/clutch codes
-condition_master %>%
+bio_dat %>%
   mutate(CW = as.numeric(CW),
       maturity = case_when((Sex == 2 & CH_CC > 0) ~ 1,
                               (Sex == 2 & CH_CC == 0) ~ 0,
@@ -37,7 +38,7 @@ ebs_haul %>%
          bottom_depth, gear_temperature) %>%
   distinct() -> snow_haul
 
-#Join haul and condition datasets 
+#Join haul and biometric datasets 
 cond_mat %>% 
   rename_with(tolower) %>%
   left_join(snow_haul, by = c("cruise", "vessel", "haul")) -> mat_haul
@@ -58,9 +59,44 @@ ebs_haul %>%
 regions <- read.csv("./data/regions_lookup.csv")
   #join
 snow_cpue %>%
-  left_join(regions, by="gis_station") -> final.dat
+  left_join(regions, by="gis_station") -> snow_cpue_area
 
 ##################################################
+#Joining Lipid Lab 2/9/23 FA data (2019 and 2021 only)
 
-#Write new master csv                               
-write_csv(final.dat, file="./data/condition_haul_master.csv")
+lipid <- read.csv("./data/2019_2022 FA data.csv")
+colnames(lipid)<-gsub("X","",colnames(lipid))
+
+#Data wrangling
+lipid %>%
+  pivot_longer(!vial_id, names_to= "id", values_to = "data") %>% 
+  pivot_wider(names_from ="vial_id", values_from="data") %>%
+  mutate(vial_id = gsub(".","-",id, fixed = TRUE)) %>%
+  select(-id, -Order_processed, -Instd_Vial) %>%
+  mutate(year = as.numeric(year)) -> lipid.dat
+
+#Create % Weight FA Master by joining to haul data 
+lipid.dat %>%
+  select(contains(c("_percWT","vial_id","year"))) %>%
+  full_join(snow_cpue_area, by=c("vial_id", "year")) %>%
+  write_csv(file="./data/percWT_FA_master.csv")
+
+#Create FA per WWT Master by joining to haul data
+lipid.dat %>%
+  select(contains(c("_perWWT","vial_id","year"))) %>%
+  full_join(snow_cpue_area, by=c("vial_id","year")) %>%
+  write_csv(file="./data/perWWT_FA_master.csv")
+
+#Create Total FA Master by joining to haul data
+lipid.dat %>%
+  select(-contains(c("_perWWT","_percWT"))) %>%
+  full_join(snow_cpue_area, by=c("vial_id","year")) %>%
+  #calculate additional WWT:DWT/FA metrics
+  mutate(DWT_WWT = hepato_dwt/hepato_wwt,
+         Perc_DWT = DWT_WWT*100,
+         Total_FA = as.numeric(Total_FA_Conc)/DWT_WWT,
+         WWT_DWT = hepato_wwt/hepato_dwt) %>%
+write_csv(file="./data/total_FA_master.csv")
+
+
+
