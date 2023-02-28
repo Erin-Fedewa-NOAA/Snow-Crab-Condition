8c8484a3054fb5ecc58768e9808ecf485ee2f356
Erin-Fedewa-NOAA <Erin.Fedewa@nmfs.local>
2023-02-27 16:32:00 -0900

    Data exploration and prelim model 1

---
+++ script/bayes modeling.R
@@ -0,0 +1,153 @@
+#Analyze condition metrics in C.opilio using Bayesian multivariate models
+
+#To do: Need to append start date to include Julian day
+#Fourth root transform cpue
+#Date/location correct condition data with GAM
+
+# Author: Erin Fedewa
+# last updated: 2/27/23
+
+# load ----
+library(tidyverse)
+library(lubridate)
+library(rstan)
+library(brms)
+library(bayesplot)
+library(marginaleffects)
+library(emmeans)
+library(MARSS)
+library(corrplot)
+library(factoextra)
+library(patchwork)
+library(modelr)
+library(broom.mixed)
+library(pROC)
+library(ggthemes)
+library(tidybayes)
+library(RColorBrewer)
+library(knitr)
+library(loo)
+library(sjPlot)
+source("./script/stan_utils.R")
+
+condition_master <- read.csv("./data/total_FA_master.csv")
+
+################################
+#data wrangling  
+condition_master %>%
+  filter(lme != "NA", #one crab collected outside the sampling design
+         !vial_id %in% c("2019-65","2019-67","2019-68","2019-71","2019-66"),
+         maturity != 1,
+         Perc_DWT != "NA") %>%
+  mutate(year = as.factor(year),
+         sex = as.factor(sex),
+         region = as.factor(region),
+         station = as.factor(gis_station),
+         temperature = as.numeric(gear_temperature),
+         fourth.root.cpue = as.numeric(cpue^0.25)) -> model.dat 
+
+#Assess collinearity b/w covariates 
+model.dat %>%
+  group_by(year, station) %>%
+  summarise(temperature = mean(temperature),
+            latitude = mean(mid_latitude),
+            fourth.root.cpue = mean(fourth.root.cpue)) -> corr.dat
+
+cor(corr.dat[,3:5]) #All < 0.5
+corrplot(cor(corr.dat[,3:5]), method = 'number') 
+
+#Distribution of response variable- check family!
+model.dat %>%
+  ggplot(aes(Perc_DWT)) + 
+  geom_histogram()
+
+#############################################
+#Prelim model 1: sex, temperature, cpue
+
+mod1_formula <-  bf(Perc_DWT ~ sex + s(temperature, k = 4) + 
+                      s(fourth.root.cpue, k=4) + (1 | year/region)) 
+
+mod1 <- brm(mod1_formula,
+               data = model.dat,
+                family = gaussian,
+               cores = 4, chains = 4, iter = 2500,
+               save_pars = save_pars(all = TRUE),
+               control = list(adapt_delta = 0.999, max_treedepth = 14))
+
+#Save output
+saveRDS(mod1, file = "./output/mod1.rds")
+mod1 <- readRDS("./output/mod1.rds")
+
+#MCMC convergence diagnostics 
+check_hmc_diagnostics(mod1$fit)
+neff_lowest(mod1$fit)
+rhat_highest(mod1$fit)
+summary(mod1)
+bayes_R2(mod1)
+
+#Diagnostic Plots
+plot(mod1, ask = FALSE)
+plot(conditional_smooths(mod1), ask = FALSE)
+mcmc_plot(mod1, type = "areas", prob = 0.95)
+mcmc_rhat(rhat(mod1)) #Potential scale reduction: All rhats < 1.1
+mcmc_acf(mod1, pars = c("b_Intercept", "bs_ssize_1", "bs_sjulian_1"), lags = 10) #Autocorrelation of selected parameters
+mcmc_neff(neff_ratio(mod1)) #Effective sample size: All ratios > 0.1
+
+##########################
+
+##CPUE condition effect plot
+## 95% CI
+ce1s_1 <- conditional_effects(mod1 , effect = "fourth.root.cpue", re_formula = NA,
+                              probs = c(0.025, 0.975))
+## 90% CI
+ce1s_2 <- conditional_effects(mod1 , effect = "fourth.root.cpue", re_formula = NA,
+                              probs = c(0.05, 0.95))
+## 80% CI
+ce1s_3 <- conditional_effects(mod1 , effect = "fourth.root.cpue", re_formula = NA,
+                              probs = c(0.1, 0.9))
+dat_ce <- ce1s_1$fourth.root.cpue
+dat_ce[["upper_95"]] <- dat_ce[["upper__"]]
+dat_ce[["lower_95"]] <- dat_ce[["lower__"]]
+dat_ce[["upper_90"]] <- ce1s_2$fourth.root.cpue[["upper__"]]
+dat_ce[["lower_90"]] <- ce1s_2$fourth.root.cpue[["lower__"]]
+dat_ce[["upper_80"]] <- ce1s_3$fourth.root.cpue[["upper__"]]
+dat_ce[["lower_80"]] <- ce1s_3$fourth.root.cpue[["lower__"]]
+
+ggplot(dat_ce, aes(x = effect1__, y = estimate__)) +
+  geom_ribbon(aes(ymin = lower_95, ymax = upper_95), fill = "#F7FBFF") +
+  geom_ribbon(aes(ymin = lower_90, ymax = upper_90), fill = "#DEEBF7") +
+  geom_ribbon(aes(ymin = lower_80, ymax = upper_80), fill = "#C6DBEF") + 
+  geom_line(size = 1, color = "black") +
+  geom_point(data = model.dat, aes(x = fourth.root.cpue, y = Perc_DWT), colour = "grey80", shape= 73, size = 2) + #raw data
+  labs(x = "CPUE", y = "") +
+  theme_bw() 
+
+##Temperature 
+## 95% CI
+ce1s_1 <- conditional_effects(mod1 , effect = "temperature", re_formula = NA,
+                              probs = c(0.025, 0.975))
+## 90% CI
+ce1s_2 <- conditional_effects(mod1 , effect = "temperature", re_formula = NA,
+                              probs = c(0.05, 0.95))
+## 80% CI
+ce1s_3 <- conditional_effects(mod1 , effect = "temperature", re_formula = NA,
+                              probs = c(0.1, 0.9))
+dat_ce <- ce1s_1$temperature
+dat_ce[["upper_95"]] <- dat_ce[["upper__"]]
+dat_ce[["lower_95"]] <- dat_ce[["lower__"]]
+dat_ce[["upper_90"]] <- ce1s_2$temperature[["upper__"]]
+dat_ce[["lower_90"]] <- ce1s_2$temperature[["lower__"]]
+dat_ce[["upper_80"]] <- ce1s_3$temperature[["upper__"]]
+dat_ce[["lower_80"]] <- ce1s_3$temperature[["lower__"]]
+
+ggplot(dat_ce, aes(x = effect1__, y = estimate__)) +
+  geom_ribbon(aes(ymin = lower_95, ymax = upper_95), fill = "#F7FBFF") +
+  geom_ribbon(aes(ymin = lower_90, ymax = upper_90), fill = "#DEEBF7") +
+  geom_ribbon(aes(ymin = lower_80, ymax = upper_80), fill = "#C6DBEF") + 
+  geom_line(size = 1, color = "black") +
+  geom_point(data = model.dat, aes(x = temperature, y = Perc_DWT), colour = "grey80", shape= 73, size = 2) + #raw data
+  labs(x = "Temperature (C)", y = "") +
+  theme_bw() 
+
+
+
--- script/data exploration.R
+++ script/data exploration.R
@@ -1,7 +1,7 @@
-#Data exploration plots & data summaries 
+#Data exploration plots & data summaries for total FA dataset
 
 # Author: Erin Fedewa
-# last updated: 1/13/23
+# last updated: 2/27/23
 
 # load ----
 library(tidyverse)
@@ -9,11 +9,12 @@ library(sf)
 library(ggmap)
 library(gganimate)
 
-condition_master <- read.csv("./data/condition_haul_master.csv")
+condition_master <- read.csv("./data/total_FA_master.csv")
 
 ####################################
-#plot sampling locations by year 
+#SAMPLE SIZES AND SPATIAL EXTENT
 
+#plot sampling locations by year 
 #Basemaps
 usa <- raster::getData("GADM", country = c("USA"), level = 1, path = "./data")
 can <- raster::getData("GADM", country = c("CAN"), level = 1, path = "./data")
@@ -29,32 +30,49 @@ condition_master %>%
   theme_bw() +
   facet_wrap(~year)
 
-#2022 only
-condition_master %>% 
-  filter(year == 2022) %>%
-  group_by(mid_latitude, mid_longitude) %>%
-  summarise(n_crab=n()) %>%
-  ggplot() + 
-  geom_polygon(data = usa, aes(x = long, y = lat, group = group))+
-  geom_point(aes(x = mid_longitude, y = mid_latitude, size=n_crab), color= "light blue")+
-  coord_quickmap(xlim = c(-179, -158), ylim = c(53, 66)) +
+# Sample sizes by year 
+condition_master %>%
+  group_by(year,lme) %>%
+  count() %>%
+  filter(lme != "NA") %>% #one crab collected outside the sampling design
+  ggplot() +
+  geom_bar(aes(x=as.factor(lme), y= n), stat='identity') +
+  facet_wrap(~year) +
   theme_bw() +
-  labs(x="", y="", size="no. of crab")
+  labs(x= "", y = "Sample size")
 
-# Table of sample sizes by sampling region 
+# Sample sizes by sampling region 
 condition_master %>%
   group_by(year,region, lme) %>%
   count() %>%
-  print(n=50) -> plot
-
-#Plot of sample sizes
-plot %>%
   filter(lme != "NA") %>% #one crab collected outside the sampling design
   ggplot() +
   geom_bar(aes(x=as.factor(region), y= n), stat='identity') +
   facet_grid(lme~year) +
   theme_bw() +
   labs(x= "Bering Sea Region", y = "Sample size")
+
+#Sample sizes by maturity
+condition_master %>%
+  filter(maturity != "NA") %>%
+  group_by(year,maturity) %>%
+  count() %>%
+  ggplot() +
+  geom_bar(aes(x=as.factor(maturity), y= n), stat='identity') +
+  facet_wrap(~year) +
+  theme_bw() +
+  labs(x= "Maturity Status", y = "Sample size") #22 mature males in dataset
+
+#Sample sizes by sex
+condition_master %>%
+  filter(sex != "NA") %>%
+  group_by(year,sex) %>%
+  count() %>%
+  ggplot() +
+  geom_bar(aes(x=as.factor(sex), y= n), stat='identity') +
+  facet_wrap(~year) +
+  theme_bw() +
+  labs(x= "Sex", y = "Sample size")
 
 #Size composition sampled by region/yr
 condition_master %>%
@@ -67,4 +85,217 @@ condition_master %>%
   facet_grid(lme~year) +
   theme_bw() +
   labs(x= "Snow crab carapace width (mm)", y = "Count")
+
+#Pull very large females that appear to be Tanner crab and re-plot
+condition_master %>%
+  mutate(Sex = recode_factor(sex, '1' = "M", '2' = "F")) %>%
+  filter(lme != "NA", #one crab collected outside the sampling design
+         !vial_id %in% c("2019-65","2019-67","2019-68","2019-71","2019-66")) %>% #large females likely tanners
+  group_by(year) %>%
+  ggplot() +
+  geom_histogram(aes(x=cw, fill=Sex), position = "stack", binwidth = 2) +
+  scale_fill_manual(values=c("#00BFC4", "#F8766D")) +
+  facet_grid(lme~year) +
+  theme_bw() +
+  labs(x= "Snow crab carapace width (mm)", y = "Count")
+#will need to factor size into models! 
+
+#Size range sampled across years by region
+condition_master %>%
+  filter(lme != "NA", #one crab collected outside the sampling design
+         !vial_id %in% c("2019-65","2019-67","2019-68","2019-71","2019-66")) %>%
+  group_by(lme, sex) %>%
+  summarize(avg_cw = mean(cw, na.rm=T), 
+            max_cw = max(cw, na.rm=T), 
+            min_cw = min(cw, na.rm=T))
+
+#############################################
+#RXN BETWEEN CONDITION METRICS 
+
+#data wrangling 
+condition_master %>%
+  filter(lme != "NA", #one crab collected outside the sampling design
+         !vial_id %in% c("2019-65","2019-67","2019-68","2019-71","2019-66"),
+         maturity != 1) -> new.dat
+
+#% DWT vrs total FA
+new.dat %>%
+  ggplot(aes(Perc_DWT, Total_FA, color=factor(year))) +
+  geom_point() +
+  theme_bw() + 
+  geom_smooth(method = "lm", se = FALSE) +
+  labs(x= "% DWT in Hepatopancreas", y = "Total FA (mg/g DWT)")
+#two major outliers from 2019 may need a closer look 
+
+#% DWT vrs total FA concentration
+new.dat %>%
+  ggplot(aes(Perc_DWT, Total_FA_Conc, color=factor(year))) +
+  geom_point() +
+  theme_bw() + 
+  geom_smooth(method = "lm", se = FALSE) +
+  labs(x= "% DWT in Hepatopancreas", y = "Total FA per WWT (mg FA/g WWT)")
+#two major outliers from 2019 may need a closer look 
+
+#Crab weight vrs % DWT by sex
+new.dat %>%
+  ggplot(aes(crab_wgt, Perc_DWT, color=factor(year))) +
+  geom_point() +
+  theme_bw() + 
+  geom_smooth(method = "lm", se = FALSE) +
+  labs(x= "Crab weight (g)", y = "% DWT in hepatopancreas") +
+  facet_wrap(~sex, scales = "free_x")
+#Lots of indvidual variability! Might not expect a strong size/weight
+  # rxn with FA's because growth is incremental and we only sampled one cohort? 
+
+#Crab size vrs % DWT by sex
+new.dat %>%
+  ggplot(aes(cw, Perc_DWT, color=factor(year))) +
+  geom_point() +
+  theme_bw() + 
+  geom_smooth(method = "lm", se = FALSE) +
+  labs(x= "Carapace width (mm)", y = "% DWT in hepatopancreas") +
+  facet_wrap(~sex, scales = "free_x")
+
+# Condition factor K vrs % DWT
+new.dat %>%
+mutate(K=crab_wgt/(cw^3)*100000) %>%
+  ggplot(aes(K, Perc_DWT, color=factor(year))) +
+  geom_point() +
+  theme_bw()  +
+  geom_smooth(method = "lm", se = FALSE) +
+  labs(x= "Fultons K Condition Factor", y = "% DWT in hepatopancreas") +
+  facet_wrap(~sex, scales = "free_x")
+#I believe there's a better way to do this though...follow up
+
+#############################################
+#SPATIAL/INTERANNUAL VARIATION IN CONDITION METRICS 
+  #Using %DWT for now as proxy until 2022 data comes in
+
+#%DWT FA by lme and year
+new.dat %>%
+  ggplot(aes(factor(year), Perc_DWT)) +
+  geom_boxplot() +
+  facet_wrap(~lme) +
+  theme_bw() +
+  labs(x= "", y = "% DWT in hepatopancreas")
+
+#%DWT by region and year
+new.dat %>%
+  ggplot(aes(factor(region), Perc_DWT)) +
+  geom_boxplot() +
+  facet_wrap(~year) +
+  theme_bw() +
+  labs(x= "", y = "% DWT in hepatopancreas")
+
+#%DWT by lme, year and sex
+new.dat %>%
+  ggplot(aes(factor(sex), Perc_DWT)) +
+  geom_boxplot() +
+  facet_grid(lme~year) +
+  theme_bw() +
+  labs(x= "", y = "% DWT in hepatopancreas")
+#EBS large males in 2019 are the anomaly 
+
+#%DWT x size by year and region
+new.dat %>%
+  ggplot(aes(cw, Perc_DWT, color=factor(year))) +
+  geom_point() +
+  theme_bw() + 
+  labs(x= "Carapace width (mm)", y = "% DWT in hepatopancreas") +
+  facet_wrap(~lme, scales = "free_x")
+
+#Mean total FA by size bin
+  new.dat %>%
+  filter(lme != "NA", #one crab collected outside the sampling design
+         !vial_id %in% c("2019-65","2019-67","2019-68","2019-71","2019-66")) %>%
+  mutate(size_bin = cut(cw, breaks=c(10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110))) %>%
+  group_by(size_bin, year, sex) %>%
+  summarise(Avg_DWT = mean(Perc_DWT, na.rm=T)) %>%
+  filter(size_bin != "NA") %>%
+  ggplot(aes(as.factor(size_bin), Avg_DWT)) +
+  geom_col() +
+  theme_bw() +
+  facet_grid(sex~year) +
+  labs(x= "", y = "% DWT Hepatopancreas") 
+
+################################################
+#RELATIONSHIPS WITH COVARIATES
+  
+#range of observed temperature data by year 
+condition_master %>%
+  group_by(year, region, gis_station) %>%
+  summarise(temperature = mean(gear_temperature)) %>%
+  ggplot(aes(temperature)) +
+  geom_histogram(bins = 12, fill = "dark grey", color = "black") +
+  facet_wrap(~year)
+
+#Depth
+condition_master %>%
+    group_by(year, region, gis_station) %>%
+    summarise(depth = mean(bottom_depth)) %>%
+    ggplot(aes(depth)) +
+    geom_histogram(bins = 12, fill = "dark grey", color = "black") +
+    facet_wrap(~year)
+
+#Cpue
+condition_master %>%
+  group_by(year, region, gis_station) %>%
+  summarise(cpue = mean(cpue)) %>%
+  ggplot(aes(cpue)) +
+  geom_histogram(fill = "dark grey", color = "black") +
+  facet_wrap(~year)
+
+#Plot explanatory variables as predictors of %DWT by year/station 
+new.dat %>%
+  group_by(year, lme, gis_station) %>%
+  summarise(size = mean(cw), 
+            temperature = mean(gear_temperature),
+            CPUE = mean(cpue^0.25), #fourth root transform
+            avg_percDWT = mean(Perc_DWT)) -> plot
+
+#Mean size-at-station vrs %DWT 
+ggplot(plot, aes(size, avg_percDWT)) +
+  geom_point() + 
+  facet_grid(lme~year) +
+  geom_smooth(method = "gam") 
+
+#Temp-at-station vrs %DWT
+ggplot(plot, aes(temperature, avg_percDWT)) +
+  geom_point() + 
+  facet_grid(lme~year) +
+  geom_smooth(method = "gam")
+
+#CPUE-at-station vrs %DWT
+ggplot(plot, aes(CPUE, avg_percDWT)) +
+  geom_point() + 
+  facet_grid(lme~year) +
+  geom_smooth(method = "gam")
+
+#Maybe we should split out by sex?
+
+################################################
+#SPATIAL PLOTS
+
+#Avg total FA by station/year
+condition_master %>% 
+  group_by(year, mid_latitude, mid_longitude) %>%
+  summarise(avg_FA=mean(Total_FA)) %>%
+  ggplot() + 
+  geom_polygon(data = usa, aes(x = long, y = lat, group = group))+
+  geom_point(aes(x = mid_longitude, y = mid_latitude, size=avg_FA), color= "light blue")+
+  coord_quickmap(xlim = c(-179, -158), ylim = c(53, 66)) +
+  theme_bw() +
+  facet_wrap(~year)
+
+##########################################
+##TO NOTE:
+#In 2019, there are 23 crab samples that have no associated FA data. Labels were lost 
+  #during shipment of these samples so FA were not run
+#Note that there are a few BCS visually positive crab noted. These crab were not extreme outliers 
+  #so were not removed from further analyses 
+#Despite protocols specifying immature males, there are 22 mature males in the dataset. These should be
+  #removed from further analyses to control for ontogeny.
+#The following vial ID's should be removed from further analyses. Crab appear to either be mature females
+  #or female tanner crab based on size: 2019-65, 2019-67, 2019-68, 2019-71, 2019-66
+
 
