8b9de516df8d76a24dba033c61918a9e7f9fe2bd
Erin-Fedewa-NOAA <Erin.Fedewa@nmfs.local>
2024-01-25 15:46:19 -0900

    Creating a CPUE plot for AMSS talk

--- script/change cpue plot.R
+++ script/change cpue plot.R
@@ -4,6 +4,7 @@
 library(tidyverse)
 library(sf)
 library(ggmap)
+library(scales) 
 
 #data
 ebs_haul <- read.csv("./data/haul_opilio.csv")
@@ -15,40 +16,53 @@ ebs_haul %>%
   mutate(year = as.numeric(str_extract(cruise, "\\d{4}"))) %>%
   group_by(year, gis_station, area_swept) %>% 
   summarise(cpue = sum(sampling_factor, na.rm = T) / mean(area_swept)) %>%
-  ungroup() -> dat
-
-dat %>%
+  ungroup() %>%
   dplyr::select(-area_swept) %>%
-  pivot_wider(names_from = year, values_from = cpue) %>%
+  mutate(cpue = cpue + 1) %>%
+  mutate(log_cpue = log(cpue))-> dat
+
+#calculate % change in CPUE
+dat %>%
+  pivot_wider(names_from = year, values_from = cpue) -> dat2
   mutate(perc_change_cpue = (((`2021` - `2018`)/`2018`)*100)) %>%
   mutate_if(is.numeric, ~ replace_na(., 0) %>% 
               replace(., is.infinite(.), 100)) %>%
   dplyr::select(-c(`2018`,`2021`)) -> cpue
-
+  
+#Extract lat and longs for stations
 ebs_haul %>%
   rename_with(tolower) %>%
-  filter(cruise == 202101,
+  mutate(year = as.numeric(str_extract(cruise, "\\d{4}"))) %>%
+  filter(cruise %in% c(201801, 202101),
          haul_type==3) %>%
-  dplyr::select(gis_station, mid_latitude, mid_longitude) %>%
-  distinct(gis_station, mid_latitude, mid_longitude) -> lat
+  dplyr::select(year, gis_station, mid_latitude, mid_longitude) %>%
+  distinct(year, gis_station, mid_latitude, mid_longitude) -> lat
 
-cpue %>%
+dat %>%
   full_join(lat) -> plot
 
-#plot % change in cpue
+#plot change in cpue
 #Basemaps
 usa <- raster::getData("GADM", country = c("USA"), level = 1, path = "./data")
 can <- raster::getData("GADM", country = c("CAN"), level = 1, path = "./data")
 
-#plot
+#plot % change in cpue
 plot %>% 
   ggplot() + 
   geom_polygon(data = usa, aes(x = long, y = lat, group=group))+
-  geom_point(aes(x = mid_longitude, y = mid_latitude, fill=as.numeric(perc_change_cpue)))+
-  coord_quickmap(xlim = c(-179, -158), ylim = c(53, 64)) +
-  theme_bw() 
+  geom_point(aes(x = mid_longitude, y = mid_latitude, color=as.numeric(cpue), size=as.numeric(cpue)))+
+  coord_quickmap(xlim = c(-178.5, -158), ylim = c(55, 62.5)) +
+  theme_bw() +
+  facet_wrap(~year) +
+  xlab("") + ylab("") + 
+  scale_color_continuous(name = "Snow Crab Density", labels = comma,
+    limits=c(0, 2850000), breaks=seq(0, 2850000, by=570000)) +
+  guides(color= guide_legend(), size=guide_legend()) +
+  scale_size_continuous(name = "Snow Crab Density", labels = comma,
+      limits=c(0, 2850000), breaks=seq(0, 2850000, by=570000))
+ggsave("./figures/data exploration/cpue.png", dpi=300)
 
 #The scale isn't showing negative values b/c data are skewed 
 #Calucalte breakpoints or scale (discrete colors vrs continuous)
 
-plot %>% print(n=500)
+dat2 %>% print(n=500)
