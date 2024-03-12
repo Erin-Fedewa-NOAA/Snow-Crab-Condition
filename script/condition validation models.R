886b5f97e41ecc7ca65d946ca65b4d9e0d15638a
Erin-Fedewa-NOAA <Erin.Fedewa@nmfs.local>
2024-03-11 17:41:44 -0800

    Final condition validation bayes model run

--- script/condition validation models.R
+++ script/condition validation models.R
@@ -32,13 +32,14 @@ source("./script/stan_utils.R")
 condition_master <- read.csv("./data/total_FA_master.csv")
 
 ####################################
-#Hepato %DWT vrs Total Fatty Acids
+#Hepato %DWT condition metric 
 
 #data wrangling 
 condition_master %>%
   filter(lme != "NA", #one crab collected outside the sampling design
          !vial_id %in% c("2019-65","2019-67","2019-68","2019-71","2019-66","2019-207", "2019-212"),#likely tanners
-         maturity != 1) -> new.dat
+         maturity != 1,
+         year > 2019) -> new.dat
 
 #Let's look at distributions
 condition_master %>%
@@ -62,7 +63,6 @@ new.dat %>%
 cbPalette <- c("#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00")
 
 new.dat %>%
-  filter(year > 2019) %>%
   ggplot(aes(Perc_DWT, Total_FA_Conc_DWT)) +
   geom_point(aes(color=factor(year))) +
   theme_bw() + 
@@ -72,10 +72,11 @@ new.dat %>%
   scale_colour_manual(values=cbPalette)
 #facet_wrap(~lme)
 
-#Bayesian regression model 
+#########
+#Bayesian regression model (FA ~ %DWT)
 condition_1 <- brm(data = new.dat,
-            family = student,
-            Total_FA_Conc_DWT ~ Perc_DWT,
+            family = gaussian,
+            Total_FA_Conc_DWT ~ 1 + Perc_DWT,
             seed=1,
             save_pars = save_pars(all = TRUE),
             control = list(adapt_delta = 0.999, max_treedepth = 14))
@@ -87,31 +88,43 @@ condition_1 <- readRDS("./output/condition_1.rds")
 summary(condition_1)
 bayes_R2(condition_1)
 posterior_summary(condition_1)
+pairs(condition_1)
 loo_c1 <- loo(condition_1)
 pareto_k_table(loo_c1)
 
 #Diagnostic Plots
 plot(condition_1, ask = FALSE)
 
+#Predictions
+dwt_seq <- tibble(Perc_DWT = seq(from = 0, to = 60, by =1))
 
+mu <- fitted(condition_1, newdata = dwt_seq) %>%
+  as_tibble() %>%
+  bind_cols(dwt_seq)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-#% Linear regression: DWT vrs total FA concentration 
+#Plot model fit - Fig 2 for Manuscript 
 new.dat %>%
-  filter(year > 2019) -> dat_sub
-mod1 <- lm(Total_FA_Conc_DWT~Perc_DWT, data=dat_sub) 
+  ggplot(aes(x = Perc_DWT, y = Total_FA_Conc_DWT)) +
+  #geom_abline(intercept = fixef(condition_1)[1], 
+             # slope     = fixef(condition_1)[2],
+             # size = .8, color = "black") +
+  geom_smooth(data = mu, aes(y=Estimate, ymin= Q2.5, ymax= Q97.5), 
+              stat = "identity", fill = "lightgrey", color = "black", alpha = 0.5) +
+  geom_point(aes(color=as.factor(year)), size = 2) +
+  theme_bw() +
+  theme(panel.grid = element_blank()) +
+  theme_bw() + 
+  labs(x= "% DWT in Hepatopancreas", y = "Total FA per DWT (mg FA/g WWT)") +
+  theme(legend.title=element_blank()) +
+  scale_colour_manual(values=cbPalette) +
+  coord_cartesian(xlim = range(new.dat$Perc_DWT),
+                  ylim = c(0,775)) 
+ggsave("./figures/Fig2.png")
+ 
+ ######
+
+# Linear regression (FA ~ %DWT) 
+mod1 <- lm(Total_FA_Conc_DWT~Perc_DWT, data=new.dat) 
 summary(mod1)
 plot(mod1)
 
@@ -125,8 +138,8 @@ influential <- cooksD[(cooksD > (3 * mean(cooksD, na.rm = TRUE)))]
 influential #46 points with Cook's Distance greater than 3x the mean, mostly in 2021
 
 names_of_influential <- names(influential)
-outliers <- dat_sub[names_of_influential,]
-dat_without_outliers <- dat_sub %>% anti_join(outliers)
+outliers <- new.dat[names_of_influential,]
+dat_without_outliers <- new.dat %>% anti_join(outliers)
 
 #Re-fit model with outliers removed 
 mod2 <- lm(Total_FA_Conc_DWT~Perc_DWT, data=dat_without_outliers) 
@@ -145,6 +158,7 @@ dat_without_outliers %>%
   glance()
 
 #####################################
+#Morphometric condition metrics
 
 #Crab weight at size vrs % DWT by sex
 new.dat %>%
@@ -159,7 +173,7 @@ new.dat %>%
 #% LM: weight/size ratio vrs total FA concentration - no 2019
 new.dat %>%
   filter(year > 2019,
-         !vial_id %in% c("2023-147", "2022-AKK-175")) %>% #outliers based on wgt- likely back deck errors-> dat_sub
+         !vial_id %in% c("2023-147", "2022-AKK-175")) %>% #outliers based on wgt- likely back deck errors-> new.dat
   mutate(lw = crab_wgt/cw) -> wgt_dat
 mod2 <- lm(Total_FA_Conc_DWT~lw, data=wgt_dat) 
 summary(mod2)
