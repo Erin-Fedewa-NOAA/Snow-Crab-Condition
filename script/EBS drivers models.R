ec0fc7c3a4533e47ee7d260336a0d55522f76d1e
Erin-Fedewa-NOAA <Erin.Fedewa@nmfs.local>
2024-03-22 09:57:17 -0800

    Model updates

--- script/EBS drivers models.R
+++ script/EBS drivers models.R
@@ -7,6 +7,12 @@
 # Author: Erin Fedewa
 # last updated: 3/12/24
 
+#install github version of brms with fix for truncated skew normal model runs
+#if (!requireNamespace("remotes")) {
+  #install.packages("remotes")
+#}
+#remotes::install_github("paul-buerkner/brms")
+
 # load ----
 library(tidyverse)
 library(lubridate)
@@ -29,11 +35,6 @@ library(knitr)
 library(loo)
 library(sjPlot)
 source("./script/stan_utils.R")
-
-if (!requireNamespace("remotes")) {
-  install.packages("remotes")
-}
-remotes::install_github("paul-buerkner/brms")
 
 condition_master <- read.csv("./data/total_FA_master.csv")
 
@@ -60,15 +61,16 @@ condition_master %>%
          station = as.factor(gis_station),
          temperature = as.numeric(gear_temperature),
          fourth.root.cpue = as.numeric(cpue^0.25),
-         fourth.root.invert = as.numeric(total_benthic_cpue^0.25)) -> ebs.dat 
+         fourth.root.invert = as.numeric(total_benthic_cpue^0.25),
+         julian = as.numeric(julian)) -> ebs.dat 
 
 #Assess collinearity b/w covariates 
 ebs.dat %>%
   group_by(year, station) %>%
   summarise(temperature = mean(temperature),
-            latitude = mean(mid_latitude),
             fourth.root.cpue = mean(fourth.root.cpue), 
-            fourth.root.invert = mean(fourth.root.invert)) -> corr.dat
+            fourth.root.invert = mean(fourth.root.invert), 
+            julian = mean(julian)) -> corr.dat
 
 cor(corr.dat[,3:6]) #All < 0.6
 corrplot(cor(corr.dat[,3:6]), method = 'number') 
@@ -86,25 +88,17 @@ plot(density(resid(test.1, type='deviance'))) #very long tail, much heavier than
 
 # fitting a test brms model with a Gaussian likelihood - truncating response at 0, else models predict values < 0
   #ie. responses out of bounds are discarded
-model_normal <- brm(Total_FA_Conc_WWT | trunc(lb = 0) ~ year, family = gaussian(link="identity"), data = ebs.dat)
-summary(model_normal)
+model_normal_trunc <- brm(Total_FA_Conc_WWT | trunc(lb = 0) ~ year, family = gaussian(link="identity"), data = ebs.dat)
 
 #Gaussian with log response so no < 0 predictions
 model_normal_log <- brm(log(Total_FA_Conc_WWT) ~ year, family = gaussian(link="identity"), data = ebs.dat)
 summary(model_normal_log)
 
-# fitting a test brms model with a skew normal likelihood (more flexible distribution)
-model_skew <- brm(Total_FA_Conc_WWT  ~ year, family = skew_normal(), data = ebs.dat)
-summary(model_skew)
-#Negative posterior predictions for response
-
-#Bounded skew normal 
-model_skew_bound <- brm(Total_FA_Conc_WWT | trunc(lb = 0)  ~ year, family = skew_normal(), data = ebs.dat)
-#If bounded- predictions cannot be evaluated properly at the lower bound, leading to NAs?
+# fitting a test brms model with a skew normal likelihood (more flexible distribution)- truncating response
+model_skew_trunc <- brm(Total_FA_Conc_WWT | trunc(lb = 0)  ~ year, family = skew_normal(), data = ebs.dat)
 
 #skew normal with log response 
 model_skew_log <- brm(log(Total_FA_Conc_WWT)  ~ year, family = skew_normal(), data = ebs.dat)
-pp_check(model_skew_log, ndraws = 1e2)
 
 # fitting a test brms model with a gamma likelihood
 model_gamma <- brm(Total_FA_Conc_WWT ~ year, family = "gamma", data = ebs.dat)
@@ -115,45 +109,43 @@ model_log <- brm(Total_FA_Conc_WWT ~ year, family = lognormal(), data = ebs.dat)
 summary(model_log)
 
 # posterior predictive checking
-  pp_check(model_normal, ndraws = 1e2) + pp_check(model_skew, ndraws = 1e2) +
+pp_check(model_normal_trunc, ndraws = 1e2) + pp_check(model_normal_log, ndraws = 1e2) + pp_check(model_skew_trunc, ndraws = 1e2) +
   pp_check(model_skew_log, ndraws = 1e2)+ pp_check(model_gamma, ndraws = 1e2) + pp_check(model_log, ndraws = 1e2) 
-#skew normal captures mean and variance best, none of the models picking up apparent bimodality of data 
+#truncated skew normal captures mean and variance best, none of the models picking up apparent bimodality of data 
 
 # posterior predictive checking - boxplots
-pp_check(model_normal, type = "boxplot", ndraws = 20) + pp_check(model_skew, type = "boxplot", ndraws = 20) +
+pp_check(model_normal_trunc, type = "boxplot", ndraws = 20) + pp_check(model_normal_log, type = "boxplot", ndraws = 20) + pp_check(model_skew_trunc, type = "boxplot", ndraws = 20) +
   pp_check(model_skew_log, type = "boxplot", ndraws = 20) + pp_check(model_gamma, type = "boxplot", ndraws = 20) + 
   pp_check(model_log, type = "boxplot", ndraws = 20) 
-#skew normal model predicting negative responses 
 
 #let's look at the distribution of minimum values for posterior distributions vrs data
-pp_check(model_normal, type = "stat", stat = "min") + pp_check(model_skew, type = "stat", stat = "min") +
+pp_check(model_normal_trunc, type = "stat", stat = "min") + pp_check(model_normal_log, type = "stat", stat = "min") + pp_check(model_skew_trunc, type = "stat", stat = "min") +
   pp_check(model_skew_log, type = "stat", stat = "min") + pp_check(model_gamma, type = "stat", stat = "min") + 
   pp_check(model_log, type = "stat", stat = "min")
-#skew normal is predicting negative values 
 
 #now maximum values
-pp_check(model_normal, type = "stat", stat = "max") + pp_check(model_skew, type = "stat", stat = "max") +
+pp_check(model_normal_trunc, type = "stat", stat = "max") + pp_check(model_normal_log, type = "stat", stat = "max") + pp_check(model_skew_trunc, type = "stat", stat = "max") +
   pp_check(model_skew_log, type = "stat", stat = "max") + pp_check(model_gamma, type = "stat", stat = "max") + 
   pp_check(model_log, type = "stat", stat = "max")
 #lognormal is way overshooting max 
 
 #and means
-pp_check(model_normal, type = "stat", stat = "mean") + pp_check(model_skew, type = "stat", stat = "mean") +
+pp_check(model_normal_trunc, type = "stat", stat = "mean") + pp_check(model_normal_log, type = "stat", stat = "mean") + pp_check(model_skew_trunc, type = "stat", stat = "mean") +
   pp_check(model_skew_log, type = "stat", stat = "mean") + pp_check(model_gamma, type = "stat", stat = "mean") + 
   pp_check(model_log, type = "stat", stat = "mean")
   
 #Which models are providing best predictions?
-model_normal <- add_criterion(model_normal, "waic")
-model_skew <- add_criterion(model_skew, "waic")
+model_normal_trunc <- add_criterion(model_normal_trunc, "waic")
+model_skew_trunc <- add_criterion(model_skew_trunc, "waic")
 model_gamma <- add_criterion(model_gamma, "waic")
 model_log <- add_criterion(model_log, "waic")
-  loo_compare(model_normal, model_skew, model_gamma, model_log, criterion = "waic")
-#predictive accuracy highest for gaussian truncated model- but probably b/c skew normal isn't truncated?
+  loo_compare(model_normal_trunc, model_skew_trunc, model_gamma, model_log, criterion = "waic")
+#predictive accuracy highest for skew normal truncated model
   
 #Let's include log(y) models, but we need Jacobian correction to make models comparable 
 loo_normal_log <- loo(model_normal_log)
-loo_normal_trunc <- loo(model_normal)
-loo_skew_trunc <- loo(model_skew_bound)
+loo_normal_trunc <- loo(model_normal_trunc)
+loo_skew_trunc <- loo(model_skew_trunc)
 loo_skew_log <- loo(model_skew_log)
 
 loo_normal_log_jacobian <- loo_normal_log
@@ -161,21 +153,19 @@ loo_normal_log_jacobian$pointwise[,1] <- loo_normal_log_jacobian$pointwise[,1] -
 loo_skew_log_jacobian <- loo_skew_log
 loo_skew_log_jacobian$pointwise[,1] <- loo_skew_log_jacobian$pointwise[,1] - log(ebs.dat$Total_FA_Conc_WWT)
 
-loo_compare(loo_normal_trunc, loo_normal_log_jacobian, loo_skew_log_jacobian)
-#Overall summary: The more flexible skew normal model continues to fit the data best, although a truncated
-  #skew normal model is producing NAs. More complex models with added covariates may improve data fit, 
-  #but via Stan forum consensus (https://discourse.mc-stan.org/t/choosing-a-sampling-distribution-for-left-skewed-data/34497/6)
-  #we'll roll with a skew normal distribution and log(y) response 
+loo_compare(loo_normal_trunc, loo_skew_trunc, loo_normal_log_jacobian, loo_skew_log_jacobian)
+
+#Overall summary: The more flexible skew normal model continues to fit the data best, and 
+  #visually, a truncated skew normal model appears like the best choice
+#HOWEVER, brms cannot yet compute posterior draws for truncated skew normal models yet
+#Results from models with more covariates and truncated gaussian distribution appear robust to the 
+  #two distributions, and ppc plots don't look terrible, so let's go with gaussian 
 #############################################
 #EBS Models: 
 
-#to sort out- skew normal log(y) vrs skew normal truncated
-#priors?
-#model structure 
-
 #Most complex should be - or test density/temp/invert all seperate? 
-mod1_formula <-  bf(Perc_DWT ~ sex + cw + s(temperature, k = 4) + s(julian, k = 4)
-                      s(fourth.root.cpue, k=4) + s(fourth.root.invert, k=4) + (1 | year/region)) 
+mod1_formula <-  bf(Perc_DWT ~ sex + s(cw, k = 4) + s(temperature, k = 4) + s(julian, k = 4)
+                      s(fourth.root.cpue, k=4) + s(fourth.root.invert, k=4) + (1 | region/station)) 
 
 #Model 1: %dWT ~ sex, cw, temperature, cpue, benthic invert,  
 
@@ -339,8 +329,15 @@ ce1s_1$sex %>%
 ########################
 #Simpler model- pulling temp b/c that is essentially a year effect 
 
-mod2_formula <-  bf(Perc_DWT ~ sex +  
-                      s(fourth.root.cpue, k=4)  + year + (1 | region)) 
+mod2_formula <-  bf(Total_FA_Conc_WWT | trunc(lb = 0) ~ s(cw, k = 4) + s(temperature, k = 4, by = year) + s(julian, k = 4) +
+                      s(fourth.root.cpue, k=4, by = year)  + (1 | station)) 
+
+set_priors <- c(set_prior("normal(0, 3)", class = "b"), #slope prior
+                set_prior("normal(0, 3)", class = "Intercept"),
+                set_prior("student_t(3, 0, 2.5)", class = "sd"),
+                prior(cauchy(0, 10), class = sigma, class = "sigma"))
+
+model_priors <- c(set_prior("normal(0,1.5)","b"), set_prior("normal(0,1.5)","Intercept"))
 
 mod2 <- brm(mod2_formula,
             data = ebs.dat,
@@ -357,11 +354,71 @@ mod2 <- readRDS("./output/mod2.rds")
 check_hmc_diagnostics(mod2$fit)
 neff_lowest(mod1$fit)
 rhat_highest(mod1$fit)
+pp_check(mod2, ndraws = 1000) + labs(title = str_glue("Posterior predictive checks for mod2"))
 summary(mod2)
-bayes_R2(mod1)
+tidy(mod2)
+bayes_R2(mod2)
+a <- loo(mod2)
+plot(a)
 
+plot(marginal_effects(mod2),points=T) #does it fit the data?
+marginal_effects(mod2)
 conditional_effects(mod2)
 #condition better at high density - better habitat? 
+
+pit <- function(y, yrep) {
+  n_draws <- nrow(yrep)
+  pit <- sapply(1:length(y),
+                \(n) {
+                  mean(y[n] > yrep[, n]) +
+                    # randomized PIT for discrete y (Czado, C., Gneiting, T.,
+                    # Held, L.: Predictive model assessment for count
+                    # data. Biometrics 65(4), 1254–1261 (2009).)
+                    sample(sum(y[n] == yrep[, n]), 1) / n_draws
+                })
+  pmax(pmin(pit, 1), 0)
+}
+
+ppc_pit_ecdf(pit=pit(y = ebs.dat$Total_FA_Conc_WWT, yrep = posterior_predict(mod2))) #overdispersion
+ppc_intervals(y = ebs.dat$Total_FA_Conc_WWT, yrep = posterior_predict(mod2))
+
+#So region/station model is too flexible with overdispersion - only one obsv per region/station
+  #no apparent issues with 1/region ....but do we even need this? replication is at station level
+#what about 1/station....but this is already represented by group-level covariates 
+#was region/station loo error due to priors?
+#1/station-tail effective sample sizes too low
+#1/region - no concerning diagnostics
+#1/region/station - high r2 but high pareto k values, overdispersion
+
+
+
+#try this - posterior predictions via tidybayes
+ebs.dat %>%
+  group_by(year) %>%
+  data_grid(temperature = seq_range(temperature, n = 101)) %>%
+  add_predicted_draws(mod2, re_formula = NA, category="temperature") %>% #can use add_epred_draws() instead for posterior means
+  ggplot(aes(x = temperature, y = Total_FA_Conc_WWT, color = ordered(year), fill = ordered(year))) +
+  stat_lineribbon(aes(y = .prediction), .width = c(.95, .80, .50), alpha = 1/4) +
+  geom_point(data = ebs.dat) +
+  scale_fill_brewer(palette = "Set2") +
+  scale_color_brewer(palette = "Dark2")
+#or faceted
+  #facet_grid(.~year, space="free_x", scales="free_x")
+ame_fancy_zi_quota <- mod2 %>%
+  avg_comparisons(variables = "temperature") %>% 
+  posterior_draws()
+
+ggplot(ame_fancy_zi_quota, aes(x = draw)) +
+  stat_halfeye(.width = c(0.8, 0.95), point_interval = "median_hdi",
+               fill = "#bc3032") +
+  labs(x = "Average marginal effect of having a gender-based\nquota on the proportion of women in parliament", y = NULL,
+       caption = "80% and 95% credible intervals shown in black") +
+  theme_clean()
+
+r_fancy <- ame_fancy_zi_quota %>% median_hdi(draw) #after accounting for other covariates, FA conc
+  #of snow crab 
+
+mcmc_areas(as.matrix(mod2), regex_pars = "temperature")
 
 ##CPUE condition effect plot
 ## 95% CI
@@ -440,11 +497,11 @@ conditional_effects(mod4)
 
 #############################################
 
-mod5_formula <-  bf(Total_FA_Conc_WWT  ~ cw + s(temperature, k=4)  + (1 | region)) 
+mod5_formula <-  bf(Total_FA_Conc_WWT | trunc(lb = 0)  ~ cw + s(temperature, k=4)  + (1 | region)) 
 
 mod5 <- brm(mod5_formula,
             data = ebs.dat,
-            family = "gamma",
+            family = gaussian(),
             cores = 4, chains = 4, iter = 2500,
             save_pars = save_pars(all = TRUE),
             control = list(adapt_delta = 0.999, max_treedepth = 14))
@@ -454,14 +511,26 @@ saveRDS(mod5, file = "./output/mod5.rds")
 mod5 <- readRDS("./output/mod5.rds")
 summary(mod5)
 conditional_effects(mod5)
-pp_check(mod5, type="boxplot")
+pp_check(mod5)
 plot(conditional_smooths(mod5), ask = FALSE)
 
 check_hmc_diagnostics(mod5$fit)
 neff_lowest(mod1$fit)
 rhat_highest(mod1$fit)
-summary(mod2)
+summary(mod5)
 bayes_R2(mod5)
+
+y_obs <- ebs.dat$Total_FA_Conc_WWT #Observed values
+
+preds <- posterior_epred(mod5) #posterior draws
+auc <- apply(preds, 1, function(x) {
+  roc <- roc(y_obs, x, quiet = TRUE)
+  auc(roc)
+})
+hist(auc)
+
+ce1s_1 <- conditional_effects(mod5, effect = "temperature", re_formula = NA,
+                              probs = c(0.025, 0.975)) 
 
 #Not estimating temperature effect in first model- low condition with different
   #response to density. now to just look at model with temperature b/c
--- script/annual means models.R
+++ script/annual means models.R
@@ -1,3 +1,82 @@
+#Predict condition for each year/region (EBS/NBS)
+
 ##NOTE: WWT:DWT ratios appear to be affected by difference in sampling methods in 
 #2019. B/c total FA per WWT were not subject to the WWT:DWT discrepancy, it will be 
 #used as response variable in all further analyses. 
+
+mod3_formula <-  bf(Total_FA_Conc_WWT | trunc(lb = 0) ~ s(cw, k = 4) + year + s(julian, k = 4) + (1 | region/station)) 
+
+#seperate models for EBS and NBS, plot condition effect of year (so accounting for crab
+  #size, julian date and )
+
+#controlling for the effect of size due to difference in ontogeny/sex, seasonality 
+  #due to sampling design of survey, and spatial variation within the EBS
+
+mod3 <- brm(mod3_formula,
+            data = ebs.dat,
+            family = gaussian,
+            cores = 4, chains = 4, iter = 2500,
+            save_pars = save_pars(all = TRUE),
+            control = list(adapt_delta = 0.999, max_treedepth = 14))
+
+saveRDS(mod3, file = "./output/mod3.rds")
+mod3 <- readRDS("./output/mod3.rds")
+
+#MCMC convergence diagnostics 
+check_hmc_diagnostics(mod2$fit)
+neff_lowest(mod1$fit)
+rhat_highest(mod1$fit)
+pp_check(mod3, ndraws = 1000) + labs(title = str_glue("Posterior predictive checks for mod2"))
+summary(mod2)
+tidy(mod2)
+bayes_R2(mod3)
+
+plot(marginal_effects(mod3),points=T) #does it fit the data?
+marginal_effects(mod3)
+conditional_effects(mod3)
+
+#better model if we drop crab size?
+mod3.5_formula <-  bf(Total_FA_Conc_WWT | trunc(lb = 0) ~ year + s(julian, k = 4) + (1 | region/station)) 
+
+mod3.5 <- brm(mod3.5_formula,
+            data = ebs.dat,
+            family = gaussian,
+            cores = 4, chains = 4, iter = 2500,
+            save_pars = save_pars(all = TRUE),
+            control = list(adapt_delta = 0.999, max_treedepth = 14))
+
+saveRDS(mod3.5, file = "./output/mod3.5.rds")
+mod3.5 <- readRDS("./output/mod3.5.rds")
+
+pp_check(mod3.5, ndraws = 1000) + labs(title = str_glue("Posterior predictive checks for mod2"))
+summary(mod3.5)
+bayes_R2(mod3.5)
+
+plot(marginal_effects(mod3.5),points=T) #does it fit the data?
+marginal_effects(mod3)
+conditional_effects(mod3.5)
+
+loo(mod3, moment_match = T)
+loo(mod3.5, moment_match = T)
+loo_compare(mod3, mod3.5)
+
+m3 <- add_criterion(mod3, "waic")
+m3.5 <- add_criterion(mod3.5, "waic")
+loo_compare(mod3, mod3.5, criterion = "waic")
+
+##########################
+#Run NBS model
+#now extract annual conditional effect and plot, merge plots 
+
+
+#Conditional Effect for year 
+conditional_effects(hurdle1_tanner, effect = "year")
+
+ce1s_1 <- conditional_effects(hurdle1_tanner, effect = "year", re_formula = NA,
+                              probs = c(0.025, 0.975)) 
+ce1s_1$year %>%
+  dplyr::select(year, estimate__, lower__, upper__) %>%
+  mutate(species = "Tanner crab") -> year_tanner
+
+#######################
+#spatial effects - too small of sample sizes to look by region- prob not of interest 
